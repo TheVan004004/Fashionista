8c93cc28ae58a6c4c8134e93d95793380434395e
The Anh Nguyen <anhnguyenthe29112004@gmail.com>
2024-10-30 18:24:52 +0700

    no change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react'
-function App() {
-  const [listData, setListData] = useState([])
-  const [name, setName] = useState("")
-  useEffect(() => {
-    getData()
-  }, [])
-  const getData = async () => {
-    const url = "http://localhost:4000/api/products";
-    fetch(url)
-      .then(response => {
-        console.log(response)
-        // if (!response.ok) {
-        //   throw new Error('Network response was not ok');
-        // }
-        return response.json();
-      })
-      .then(data => {
-        setListData(data)
-      }
-      )
-      .catch(error => console.error('There was a problem with the fetch operation:', error));
-  }
-  const handleSearch = () => {
-    const url = `http://localhost:4000/api/products/filter?name=${name}`;
-    fetch(url)
-      .then(response => {
-        console.log(response)
-        // if (!response.ok) {
-        //   throw new Error('Network response was not ok');
-        // }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data)
-        setListData(data)
-      })
-  }
 
+function App() {
   return (
     <div className="App">
-      <input value={name} onChange={(e) => setName(e.target.value)}></input>
-      <button onClick={handleSearch}>search </button>
-      {
-        listData.map((data) => {
-          console.log(data)
-          return (
-            <p>
-              name: {data.name} quantity: {data.quantity}
-            </p>
-
-          )
-        })
-      }
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
