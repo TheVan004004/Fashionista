083332bf6403f1dd109dc3d4d5b9532a8b4842ea
The Van <thevan04072k4@gmail.com>
2024-12-13 10:25:16 +0700

    fix UI

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -1,132 +1,147 @@
-import { useContext, useEffect, useState } from 'react';
-import '../styles/filter.css'
-import { MainContext } from '../context/main.context';
-import Color from './Color';
-import Size from './Size';
-import { HiAdjustments, HiOutlineXCircle } from 'react-icons/hi';
-import { getAllColorAPI } from '../services/services';
+import { useContext, useEffect, useState } from "react";
+import "../styles/pages/filter.css";
+import { MainContext } from "../context/main.context";
+import Color from "./Color";
+import Size from "./Size";
+import { HiAdjustments, HiOutlineXCircle } from "react-icons/hi";
+import { getAllColorAPI } from "../services/services";
 const Filter = () => {
-    const { color, size, minPrice, maxPrice, colorFilter, setColorFiler, setMinPrice, setMaxPrice } = useContext(MainContext)
-    const [openFilter, setOpenFilter] = useState(true)
-    const [listColor, setListColor] = useState([])
-    useEffect(() => {
-        getColor()
-    }, [])
-    const getColor = async () => {
-        const res = await getAllColorAPI()
-        const data = await res.data
-        setListColor(data)
-    }
-    return (
-        <>
+  const {
+    color,
+    size,
+    minPrice,
+    maxPrice,
+    colorFilter,
+    setColorFiler,
+    setMinPrice,
+    setMaxPrice,
+  } = useContext(MainContext);
+  const [openFilter, setOpenFilter] = useState(true);
+  const [listColor, setListColor] = useState([]);
+  useEffect(() => {
+    getColor();
+  }, []);
+  const getColor = async () => {
+    const res = await getAllColorAPI();
+    const data = await res.data;
+    setListColor(data);
+  };
+  return (
+    <>
+      <HiAdjustments
+        style={{
+          position: "fixed",
+          top: "80px",
+          left: openFilter ? "-100px" : "10px",
+          fontSize: "24px",
+          cursor: "pointer",
+          zIndex: "10",
+          padding: "5px",
+          color: "var(--background-color)",
+          backgroundColor: "var(--accent-color)",
+          borderRadius: "1000px",
+          transition: "ease-in-out 0.5s",
+        }}
+        onClick={() => setOpenFilter(true)}
+      />
 
-            <HiAdjustments
-                style={{
-                    position: "fixed",
-                    top: "80px",
-                    left: openFilter ? "-100px" : "10px",
-                    fontSize: "24px",
-                    cursor: "pointer",
-                    zIndex: "10",
-                    padding: "5px",
-                    color: "var(--background-color)",
-                    backgroundColor: "var(--accent-color)",
-                    borderRadius: "1000px",
-                    transition: "ease-in-out 0.5s",
+      <div id="filterSidebar" className={openFilter ? "active" : ""}>
+        <HiOutlineXCircle
+          style={{
+            position: "absolute",
+            top: "20px",
+            right: "10px",
+            fontSize: "24px",
+            cursor: "pointer",
+          }}
+          onClick={() => setOpenFilter(false)}
+        />
+        <div className="filter-section">
+          <h4>Nhóm sản phẩm</h4>
+          <div className="filter-group">
+            <label>
+              <input type="checkbox" name="category" /> Quần áo nam
+            </label>
+            <label>
+              <input type="checkbox" name="category" /> Quần áo nữ
+            </label>
+          </div>
+        </div>
+        <div className="filter-section">
+          <h4>Khoảng giá (1.000đ) </h4>
+          <div className="filter-price">
+            <div className="price-inputs">
+              <input
+                type="number"
+                placeholder="Min"
+                value={minPrice}
+                onChange={(e) => setMinPrice(e.target.value)}
+              />
+              <span>-</span>
+              <input
+                type="number"
+                placeholder="Max"
+                value={maxPrice}
+                onChange={(e) => setMaxPrice(e.target.value)}
+              />
+            </div>
+            <div className="price-select">
+              <button
+                onClick={() => {
+                  setMaxPrice(100);
+                  setMinPrice(0);
                 }}
-                onClick={() => setOpenFilter(true)}
-            />
+              >
+                Dưới 100
+              </button>
+              <button
+                onClick={() => {
+                  setMaxPrice(200);
+                  setMinPrice(100);
+                }}
+              >
+                100 - 200
+              </button>
+              <button
+                onClick={() => {
+                  setMaxPrice(300);
+                  setMinPrice(200);
+                }}
+              >
+                200 - 300
+              </button>
+              <button
+                onClick={() => {
+                  setMinPrice(500);
+                  setMaxPrice(9999);
+                }}
+              >
+                Trên 500{" "}
+              </button>
+            </div>
+          </div>
+        </div>
 
-
-            <div id="filterSidebar" className={openFilter ? "active" : ""}
-            >
-                <HiOutlineXCircle
-                    style={{
-                        position: "absolute",
-                        top: "20px",
-                        right: "10px",
-                        fontSize: "24px",
-                        cursor: "pointer",
-                    }}
-                    onClick={() => setOpenFilter(false)}
-                />
-                <div className="filter-section">
-                    <h4>Nhóm sản phẩm</h4>
-                    <div className="filter-group">
-                        <label><input type="checkbox" name="category" /> Quần áo nam</label>
-                        <label><input type="checkbox" name="category" /> Quần áo nữ</label>
-                    </div>
-                </div >
-                <div className="filter-section">
-                    <h4>Khoảng giá (1.000đ) </h4>
-                    <div className="filter-price">
-                        <div className="price-inputs">
-                            <input type="number" placeholder="Min"
-                                value={minPrice}
-                                onChange={(e) => setMinPrice(e.target.value)}
-                            />
-                            <span>-</span>
-                            <input type="number" placeholder="Max"
-                                value={maxPrice}
-                                onChange={(e) => setMaxPrice(e.target.value)}
-                            />
-                        </div>
-                        <div className="price-select">
-                            <button
-                                onClick={() => {
-                                    setMaxPrice(100)
-                                    setMinPrice(0)
-                                }}
-                            >Dưới 100</button>
-                            <button
-                                onClick={() => {
-                                    setMaxPrice(200)
-                                    setMinPrice(100)
-                                }}
-                            >100 - 200</button>
-                            <button
-                                onClick={() => {
-                                    setMaxPrice(300)
-                                    setMinPrice(200)
-                                }}
-                            >200 - 300</button>
-                            <button
-                                onClick={() => {
-                                    setMinPrice(500)
-                                    setMaxPrice(9999)
-                                }}
-                            >Trên 500 </button>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="filter-section">
-                    <h4>Kích cỡ</h4>
-                    <div className="filter-size">
-                        <Size sizeName="M" />
-                        <Size sizeName="L" />
-                        <Size sizeName="XL" />
-                        <Size sizeName="2XL" />
-                    </div>
-                </div>
-                <div className="filter-section">
-                    <h4>Màu sắc</h4>
-                    <div className="filter-color">
-                        {
-                            listColor.map((color, index) => {
-                                return (
-                                    <Color color={color} key={color.hex_code} />
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-
-            </div >
-        </>
-    )
-
-}
+        <div className="filter-section">
+          <h4>Kích cỡ</h4>
+          <div className="filter-size">
+            <Size sizeName="M" />
+            <Size sizeName="L" />
+            <Size sizeName="XL" />
+            <Size sizeName="2XL" />
+          </div>
+        </div>
+        <div className="filter-section">
+          <h4>Màu sắc</h4>
+          <div className="filter-color">
+            {listColor.map((color, index) => {
+              return <Color color={color} key={color.hex_code} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Filter;
--- frontend/src/layout/footer.jsx
+++ frontend/src/layout/footer.jsx
@@ -1,55 +1,105 @@
-import '../styles/footer.css'
+import "../styles/layout/footer.css";
 import { FaFacebook, FaYoutube } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 const Footer = () => {
-    return (
-        <>
-            <footer className="footer">
-                <div className="footer-container">
-                    <div className="footer-section">
-                        <h3>Dịch vụ khách hàng</h3>
-                        <ul>
-                            <li><a href="#">Chính sách khách hàng thân thiết</a></li>
-                            <li><a href="#">Chính sách đổi trả</a></li>
-                            <li><a href="#">Chính sách bảo mật</a></li>
-                            <li><a href="#">Chính sách sử dụng Cookies</a></li>
-                            <li><a href="#">Chính sách thanh toán, giao nhận</a></li>
-                            <li><a href="#">Chính sách đơn đồng phục</a></li>
-                            <li><a href="#">Hướng dẫn chọn size</a></li>
-                            <li><a href="#">Đăng ký đối tác</a></li>
-                        </ul>
-                    </div>
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-section">
+            <h3>Dịch vụ khách hàng</h3>
+            <ul>
+              <li>
+                <a href="#">Chính sách khách hàng thân thiết</a>
+              </li>
+              <li>
+                <a href="#">Chính sách đổi trả</a>
+              </li>
+              <li>
+                <a href="#">Chính sách bảo mật</a>
+              </li>
+              <li>
+                <a href="#">Chính sách sử dụng Cookies</a>
+              </li>
+              <li>
+                <a href="#">Chính sách thanh toán, giao nhận</a>
+              </li>
+              <li>
+                <a href="#">Chính sách đơn đồng phục</a>
+              </li>
+              <li>
+                <a href="#">Hướng dẫn chọn size</a>
+              </li>
+              <li>
+                <a href="#">Đăng ký đối tác</a>
+              </li>
+            </ul>
+          </div>
 
-                    <div className="footer-section">
-                        <h3>Về FASHIONISTA</h3>
-                        <ul>
-                            <li><a href="#">Giới thiệu</a></li>
-                            <li><a href="#">Liên hệ</a></li>
-                            <li><a href="#">Tin tức</a></li>
-                            <li><a href="#">Hệ thống cửa hàng</a></li>
-                            <li><a href="#">Tin khuyến mãi</a></li>
-                        </ul>
-                        <h3>Địa chỉ liên hệ</h3>
-                        <p>Địa chỉ: Đại học Bách Khoa Hà Nội</p>
-                    </div>
+          <div className="footer-section">
+            <h3>Về FASHIONISTA</h3>
+            <ul>
+              <li>
+                <a href="#">Giới thiệu</a>
+              </li>
+              <li>
+                <a href="#">Liên hệ</a>
+              </li>
+              <li>
+                <a href="#">Tin tức</a>
+              </li>
+              <li>
+                <a href="#">Hệ thống cửa hàng</a>
+              </li>
+              <li>
+                <a href="#">Tin khuyến mãi</a>
+              </li>
+            </ul>
+            <h3>Địa chỉ liên hệ</h3>
+            <p>Địa chỉ: Đại học Bách Khoa Hà Nội</p>
+          </div>
 
-                    <div className="footer-section">
-                        <h3>FASHIONISTA lắng nghe bạn</h3>
-                        <p>Chúng tôi luôn trân trọng và mong đợi mọi ý kiến đóng góp từ khách hàng để có thể nâng cấp trải
-                            nghiệm dịch vụ và sản phẩm tốt hơn nữa.</p>
-                        <p><strong>Liên hệ đặt hàng</strong><br />012 345 6789</p>
-                        <p><strong>Góp ý khiếu nại</strong><br />1800 1000</p>
-                        <p><strong>Email</strong><br />chamsockhachhang@fashionista.vn</p>
-                        <div className="social-icons" style={{ display: "flex", gap: "10px" }}>
-                            <a href="#"><FaFacebook /></a>
-                            <a href="#"><RiInstagramFill /></a>
-                            <a href="#"><FaYoutube /></a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        </>
-    )
-}
+          <div className="footer-section">
+            <h3>FASHIONISTA lắng nghe bạn</h3>
+            <p>
+              Chúng tôi luôn trân trọng và mong đợi mọi ý kiến đóng góp từ khách
+              hàng để có thể nâng cấp trải nghiệm dịch vụ và sản phẩm tốt hơn
+              nữa.
+            </p>
+            <p>
+              <strong>Liên hệ đặt hàng</strong>
+              <br />
+              012 345 6789
+            </p>
+            <p>
+              <strong>Góp ý khiếu nại</strong>
+              <br />
+              1800 1000
+            </p>
+            <p>
+              <strong>Email</strong>
+              <br />
+              chamsockhachhang@fashionista.vn
+            </p>
+            <div
+              className="social-icons"
+              style={{ display: "flex", gap: "10px" }}
+            >
+              <a href="#">
+                <FaFacebook />
+              </a>
+              <a href="#">
+                <RiInstagramFill />
+              </a>
+              <a href="#">
+                <FaYoutube />
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default Footer;
